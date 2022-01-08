1c016b9fb17b44d310ecc215320cf87ccc5369af
Auvigoo <auvigooa@gmail.com>
2022-01-08 17:10:05 -0500

    Connect server to mongoDB

--- server/index.js
+++ server/index.js
@@ -8,3 +8,11 @@ const app =  express();
 app.use(bodyParser.json({ limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
 app.use(cors());
+
+//mongoDB connection
+const CONNECTION_URL = 'mongodb+srv://avg:avg123@cluster0.ptx57.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+        .then( () => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`) ) )
+        .catch( (error) => console.log(error.message));
