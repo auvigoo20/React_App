5d20d6a6664944b0acd090a02734c04e3d789c1e
Auvigoo <auvigooa@gmail.com>
2022-01-08 17:32:05 -0500

    Add logic for getPosts

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,22 @@
-export const getPosts =  (req, res) => {
-    res.send("This works!");
+import PostMessage from "../models/postMessage.js";
+
+
+
+//PostMessage.find() takes time, so make function async
+export const getPosts = async (req, res) => {
+    try {
+        const postMessages = await PostMessage.find();
+
+        console.log(postMessages);
+
+        res.status(200).json(postMessages);
+    }
+    catch (error){
+        res.status(404).json({message: error.message});
+
+    }
+}
+
+export const createPost = (req, res) => {
+
 }
