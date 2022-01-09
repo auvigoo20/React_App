dd9e982d5156e1609662bb554b28b5648e18aafd
Auvigoo <auvigooa@gmail.com>
2022-01-09 01:00:39 -0500

    Implement delete functionality

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -50,3 +50,15 @@ export const updatePost = async(req, res) => {
 
 
 }
+
+export const deletePost = async (req, res) => {
+    const {id} = req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).send('No post with that id');
+    }
+
+    await PostMessage.findByIdAndDelete(id);
+
+    res.json({message: 'Post deleted successfully'});
+}
