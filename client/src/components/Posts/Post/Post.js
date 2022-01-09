31e1143e9e6166e6102b68dad6e34da79215f639
Auvigoo <auvigooa@gmail.com>
2022-01-09 01:20:38 -0500

    Implemented likes

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -5,7 +5,7 @@ import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import moment from 'moment';
 import { useDispatch } from 'react-redux';
-import { deletePost } from '../../../actions/posts';
+import { deletePost, likePost } from '../../../actions/posts';
 
 import useStyles from './styles'
     
@@ -42,11 +42,9 @@ const Post = ( {post, setCurrentId} ) => {
             </CardContent>
             
             <CardActions className={classes.cardActions}>
-                <Button size="small" color="primary" onClick={ () => {} }>
-                    <ThumbUpAltIcon fontSize="small">
-                        Like
-                        {post.likeCount}
-                    </ThumbUpAltIcon>
+                <Button size="small" color="primary" onClick={ () => {dispatch(likePost(post._id))} }>
+                    <ThumbUpAltIcon fontSize="small" />
+                        Like {post.likeCount}
                 </Button>
                 <Button size="small" color="primary" onClick={ () => dispatch(deletePost(post._id)) }>
                     <DeleteIcon fontSize="small">
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import {getPosts, createPost, updatePost, deletePost} from '../controllers/posts.js';
+import {getPosts, createPost, updatePost, deletePost, likePost} from '../controllers/posts.js';
 
 const router = express.Router();
 
@@ -8,5 +8,6 @@ router.get('/', getPosts);
 router.post('/', createPost);
 router.patch('/:id', updatePost); //send the id to update the correct post
 router.delete('/:id', deletePost);
+router.patch('/:id/likePost', likePost) //patch request since liking is basically updating
 
 export default router;
