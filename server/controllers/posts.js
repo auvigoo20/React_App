9b088b331ca2d7e0af73020f7e50ae21b040a396
Auvigoo <auvigooa@gmail.com>
2022-01-08 21:55:05 -0500

    Add update post functionality for server side

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,4 @@
+import { Mongoose } from "mongoose";
 import PostMessage from "../models/postMessage.js";
 
 
@@ -32,4 +33,19 @@ export const createPost = async(req, res) => {
     } catch (error) {
         res.status(409).json({message : error.message});
     }
+}
+
+export const updatePost = async(req, res) => {
+    const { id: _id } = req.params;
+    const post = req.body;
+
+    if(!Mongoose.Types.ObjectId.isValid(_id)){
+        return res.status(404).send('No post with that id');
+    }
+
+    else{
+        const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, { new: true }); //update the post in the db
+    }
+
+    res.json(updatedPost);
 }
