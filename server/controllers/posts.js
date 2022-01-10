bf9bf83815b1e2d486e0b239ecdc5693cf3f217b
Auvigoo <auvigooa@gmail.com>
2022-01-09 21:07:55 -0500

    remove console log

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -8,8 +8,6 @@ export const getPosts = async (req, res) => {
     try {
         const postMessages = await PostMessage.find();
 
-        console.log(postMessages);
-
         res.status(200).json(postMessages);
     }
     catch (error){
