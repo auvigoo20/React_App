730f061d7b7e7bfb3227e08bb4f41537e4f4dd03
Auvigoo <auvigooa@gmail.com>
2022-01-09 21:05:37 -0500

    Hide mongoDB connection string

--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
 
 const app =  express();
+dotenv.config();
 
 
 
@@ -17,7 +19,7 @@ app.use(cors());
 app.use('/posts', postRoutes);
 
 //mongoDB connection
-const CONNECTION_URL = 'mongodb+srv://avg:avg123@cluster0.ptx57.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
